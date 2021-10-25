9d3423d06daf0b22e214b7318bad05bcab057eaa
Xavier Reed <xreed18@gmail.com>
2021-10-25 14:43:34 -0700

    Added homepage and sidebar components

---
+++ dashboard/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="buttons">
+        <a href="/">Home</a>
+        <a href="/">Movies</a>
+        <a href="/">TV Shows</a>
+      </div>
+    </div>
+  );
+}
