73761c5da338749a092990cfaef120220296b559
Xavier Reed <xreed18@gmail.com>
2021-10-25 18:09:44 -0700

    Added sidebar icons with react-icons

--- dashboard/src/components/Sidebar.js
+++ dashboard/src/components/Sidebar.js
@@ -1,13 +1,41 @@
 import React from "react";
 import "./Sidebar.css";
+import { HiHome } from "react-icons/hi";
+import { MdLocalMovies } from "react-icons/md";
+import { IoTv } from "react-icons/io5";
+import { IconContext } from "react-icons";
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
       <div className="buttons">
-        <a href="/">Home</a>
-        <a href="/">Movies</a>
-        <a href="/">TV Shows</a>
+        <a href="/">
+          <IconContext.Provider
+            value={{
+              className: "sidebarIcons",
+              title: "Home page",
+            }}
+          >
+            <HiHome />
+          </IconContext.Provider>
+          Home
+        </a>
+        <a href="/">
+          <IconContext.Provider
+            value={{ className: "sidebarIcons", title: "Movies page" }}
+          >
+            <MdLocalMovies />
+          </IconContext.Provider>
+          Movies
+        </a>
+        <a href="/">
+          <IconContext.Provider
+            value={{ className: "sidebarIcons", title: "TV shows page" }}
+          >
+            <IoTv />
+          </IconContext.Provider>
+          TV Shows
+        </a>
       </div>
     </div>
   );
