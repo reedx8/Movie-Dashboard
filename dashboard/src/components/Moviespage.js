29b89f3b841b54380156e34e232bdfa1bddc51f4
Austin Britton <aubrit2@pdx.edu>
2021-10-26 00:08:17 -0700

    added movies page mock up, with arbitrary color scheme

--- dashboard/src/components/Moviespage.js
+++ dashboard/src/components/Moviespage.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import './Moviespage.css';
 const Moviespage = () => {
+    let movies = ['movietitle_1','movietitle_2', 'movietitle_3', 'movietitle_1','movietitle_2', 'movietitle_3','lastmovie'];
+
     return (
         <div className="moviespage">
-            <section>
-                
-            </section>
-            <section>
+            <div id="searchContainer">
+                <input id="searchinput" type="text" placeholder="Search..."/>
+            </div>
+            <section id="newReleases">
+                <h2>New Releases</h2>
+                <div id="movieBox">
+                    {!movies ? null : movies.map((movie, key)=>{
+                    return (
+                        <div key={key} className="newReleaseMovie">
+                            <h3>{movie}</h3>
+                                
+                                
+                        </div>)
 
+
+                    })}
+                
+                </div>
             </section>
-            <aside>
+            <section id="comingSoon">
+                <h3>Coming Soon</h3>
+                    <div className="comingSoonMovie">
+                        <h3>movietitle_1</h3>
+                                    
+                    </div>
+            </section>
+            <aside id="topChart">
                 <h3>Top Chart</h3>
                 <div id="topChartGridContainer">
-                    <div class="chart_movie">
+                    <div className="topchartMovie">
                         <h5>movietitle_1</h5>
                         
                         
