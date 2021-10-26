c8e77d60f5f6321c3e8bceefd4371795eaba5a13
Austin Britton <aubrit2@pdx.edu>
2021-10-25 23:18:09 -0700

    added routing

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
 import Homepage from "./components/Homepage";
+import Moviespage from './components/Moviespage';
+import {BrowserRouter as Switch, Route, BrowserRouter} from 'react-router-dom';
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
-      <Homepage />
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' component={Homepage} exact/>
+          <Route path='/movies' component={Moviespage} exact/>
+        </Switch>
+      </BrowserRouter>
+     
     </div>
   );
 }
---
+++ dashboard/src/components/Moviespage.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Moviespage.css';
+const Moviespage = () => {
+    return (
+        <div className="moviespage">
+            <section>
+                
+            </section>
+            <section>
+
+            </section>
+            <aside>
+                <h3>Top Chart</h3>
+                <div id="topChartGridContainer">
+                    <div class="chart_movie">
+                        <h5>movietitle_1</h5>
+                        
+                        
+                    </div>
+
+
+                </div>
+            </aside>
+        </div>
+    )
+}
+
+export default Moviespage
