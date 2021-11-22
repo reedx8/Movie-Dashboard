0dd1430f83dd68e64b90536fa1175b4c8888e05f
Xavier Reed <xreed18@gmail.com>
2021-11-21 21:02:52 -0800

    fixed homepage layout

--- dashboard/src/components/Homepage.js
+++ dashboard/src/components/Homepage.js
@@ -4,15 +4,17 @@ import "./Homepage.css";
 export default function Homepage() {
   return (
     <div className="homepage">
-      <section className="mostPopular">
-        <h2>Most Popular</h2>
-      </section>
-      <section className="topRated">
-        <h2>Top Rated</h2>
-      </section>
-      <section className="comingSoon">
-        <h2>Coming Soon</h2>
-      </section>
+      <div className="subgrid">
+        <section className="mostPopular">
+          <h2>Most Popular</h2>
+        </section>
+        <section className="topRated">
+          <h2>Top Rated</h2>
+        </section>
+        <section className="comingSoon">
+          <h2>Coming Soon</h2>
+        </section>
+      </div>
     </div>
   );
 }
