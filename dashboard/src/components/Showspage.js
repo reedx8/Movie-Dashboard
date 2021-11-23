6e58dde3be9fe7867076876d991f3ff3ffd010dd
Austin Britton <aubrit2@pdx.edu>
2021-11-22 23:42:48 -0800

    added inital api call to Showspage.js

--- dashboard/src/components/Showspage.js
+++ dashboard/src/components/Showspage.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
 import {useEffect} from 'react';
-import {axios} from 'axios';
+import axios from 'axios';
 
 
 const Showspage = () => {
-  useEffect(() => {}, []);
+
+  let baseUrl = 'https://imdb8.p.rapidapi.com/auto-complete';
+  let apikey = '79dbda4576msh0d80bf6c42d69b8p12986cjsn0c3250dfa9b7';
+
+  useEffect(() => {
+
+    axios.get(baseUrl,{
+      params: {
+        q: 'game of thr'
+      },
+      headers: {
+        'x-rapidapi-host': 'imdb8.p.radidapi.com',
+        'x-rapidapi-key': apikey
+
+      }
+    }).then((response) => {
+
+      alert("response: " + JSON.stringify(response));
+    })
+
+
+  }, []);
 
   return (
     <div>
