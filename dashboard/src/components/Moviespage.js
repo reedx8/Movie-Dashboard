bd2d8e990de4b7532b8f8ba5c0536a27e1bb78d3
Austin Britton <aubrit2@pdx.edu>
2021-11-23 12:19:12 -0800

    standardized origin grid template

--- dashboard/src/components/Moviespage.js
+++ dashboard/src/components/Moviespage.js
@@ -2,16 +2,13 @@ import React from "react";
 import { useEffect } from "react";
 import axios from "axios";
 
+
 import "./Moviespage.css";
 const Moviespage = () => {
   let movies = [
     "movietitle_1",
     "movietitle_2",
-    "movietitle_3",
-    "movietitle_1",
-    "movietitle_2",
-    "movietitle_3",
-    "lastmovie",
+  
   ];
 
   let baseUrl = 'https://imdb8.p.rapidapi.com/auto-complete';
@@ -41,7 +38,9 @@ const Moviespage = () => {
   return (
     <div className="moviespage">
       <div id="searchContainer">
-        <input id="searchinput" type="text" placeholder="Search..." />
+        <label id="searchlabel" aria-label='search'>Search Movies </label>
+        <input id="searchinput" type="text" name="searchinput" placeholder="Search..." />
+       
       </div>
       <section id="newReleases">
         <h2>New Releases</h2>
@@ -57,20 +56,20 @@ const Moviespage = () => {
               })}
         </div>
       </section>
-      <section id="comingSoon">
-        <h3>Coming Soon</h3>
-        <div className="comingSoonMovie">
-          <h3>movietitle_1</h3>
-        </div>
-      </section>
       <aside id="topChart">
-        <h3>Top Chart</h3>
+        <h2>Top Chart</h2>
         <div id="topChartGridContainer">
           <div className="topchartMovie">
             <h5>movietitle_1</h5>
           </div>
         </div>
       </aside>
+      <section id="comingSoon">
+        <h3>Coming Soon</h3>
+        <div className="comingSoonMovie">
+          <h3>movietitle_1</h3>
+        </div>
+      </section>
     </div>
   );
 };
