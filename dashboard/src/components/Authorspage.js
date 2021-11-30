9db2b4760a15e6dec0449bc884344b53579c4603
Austin Britton <aubrit2@pdx.edu>
2021-11-29 20:39:01 -0800

    added spacing for photos and such

--- dashboard/src/components/Authorspage.js
+++ dashboard/src/components/Authorspage.js
@@ -3,26 +3,26 @@ import React from 'react'
 import "./Authorspage.css";
 const Authorspage = () => {
     return (
-        <div>
-            <div className={'authorspage'}>
-                <h1 style={{color: "white"}}>This is the authors page</h1>
-                <section className={'authorbox'}>
-                    <h2>name</h2>
-                    <img alt="photoguy"></img>
-                    <p>
-                        Descrption about said person
-                    </p>
-                </section>
-                <section className={'authorbox'}>
-                    <h2>name2</h2>
-                    <img alt="photoguy"></img>
-                    <p>
-                        Descrption about said person
-                    </p>
-                </section>
-                
-            </div>
+        
+        <div className={'authorspage'}>
+            <h1 id={'mainh1'}style={{color: "white"}}>This is the authors page</h1>
+            <section className={'authorbox'}>
+                <h2>name</h2>
+                <img alt="photoguy"></img>
+                <p>
+                    Descrption about said person
+                </p>
+            </section>
+            <section className={'authorbox'}>
+                <h2>name2</h2>
+                <img alt="photoguy"></img>
+                <p>
+                    Descrption about said person
+                </p>
+            </section>
+            
         </div>
+        
     )
 }
 
