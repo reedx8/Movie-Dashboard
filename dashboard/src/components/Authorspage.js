b6c6e0a7ad47b968d08e0ad063edb9bd0ea06e57
Austin Britton <aubrit2@pdx.edu>
2021-11-29 21:38:29 -0800

    finishedish layout for about us

--- dashboard/src/components/Authorspage.js
+++ dashboard/src/components/Authorspage.js
@@ -5,22 +5,28 @@ const Authorspage = () => {
     return (
         
         <div className={'authorspage'}>
-            <h1 id={'mainh1'}style={{color: "white"}}>This is the authors page</h1>
-            <section className={'authorbox'}>
-                <h2>name</h2>
-                <img alt="photoguy"></img>
-                <p>
-                    Descrption about said person
-                </p>
-            </section>
-            <section className={'authorbox'}>
-                <h2>name2</h2>
-                <img alt="photoguy"></img>
-                <p>
-                    Descrption about said person
-                </p>
-            </section>
-            
+            <h1 id={'mainh1'}>About the Authors</h1>
+            <div id={'authorcontainer'}>
+                <section className={'authorbox'}>
+                    <h2>Xavier Reed</h2>
+                    
+                    <p>
+                        Descrption about said person
+                    </p>
+                    <div className={'photoguy'}><img alt="photoguy"></img></div>
+                </section>
+                <section className={'authorbox'}>
+                    <h2>Austin Britton</h2>
+                  
+                    <p>
+                        Computer Science Major with a passion for programming. 
+                        Focused on AI/ML in the area of game theory / decision making in games without perfect imformation
+                    </p>
+                    <div className={'photoguy'}><img alt="photoguy"></img></div>
+                </section>
+            </div>
+        
+        
         </div>
         
     )
