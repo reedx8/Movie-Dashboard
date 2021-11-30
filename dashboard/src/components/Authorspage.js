7f58c6f3bd1bb7062a6a7655e477a5a1ea9ec386
Austin Britton <aubrit2@pdx.edu>
2021-11-29 20:29:55 -0800

    fixed bug

--- dashboard/src/components/Authorspage.js
+++ dashboard/src/components/Authorspage.js
@@ -1,10 +1,27 @@
 import React from 'react'
 
-import "./Showspage.css";
+import "./Authorspage.css";
 const Authorspage = () => {
     return (
         <div>
-            <h1 style={{color: "white"}}>This is the authors page</h1>
+            <div className={'authorspage'}>
+                <h1 style={{color: "white"}}>This is the authors page</h1>
+                <section className={'authorbox'}>
+                    <h2>name</h2>
+                    <img alt="photoguy"></img>
+                    <p>
+                        Descrption about said person
+                    </p>
+                </section>
+                <section className={'authorbox'}>
+                    <h2>name2</h2>
+                    <img alt="photoguy"></img>
+                    <p>
+                        Descrption about said person
+                    </p>
+                </section>
+                
+            </div>
         </div>
     )
 }
