024c931baa31bd5ca67144f8449d9ea84072a8f3
Xavier Reed <xreed18@gmail.com>
2021-12-01 13:28:52 -0800

    update authorspage

--- dashboard/src/components/Authorspage.js
+++ dashboard/src/components/Authorspage.js
@@ -1,35 +1,37 @@
-import React from 'react'
-import headshot from './authorbritton.jpg';
+import React from "react";
+import headshot from "./authorbritton.jpg";
 import "./Authorspage.css";
 const Authorspage = () => {
-    return (
-        
-        <div className={'authorspage'}>
-            <h1 id={'mainh1'}>About the Authors</h1>
-            <div id={'authorcontainer'}>
-                <section className={'authorbox'}>
-                    <h2>Xavier Reed</h2>
-                    
-                    <p>
-                        Descrption about said person
-                    </p>
-                    <div className={'photoguy'}><img alt="photoguy"></img></div>
-                </section>
-                <section className={'authorbox'}>
-                    <h2>Austin Britton</h2>
-                  
-                    <p>
-                        Computer Science Major with a passion for programming. 
-                        Focused on AI/ML in the area of game theory / decision making in games without perfect imformation
-                    </p>
-                    <div className={'photoguy'}><img id={"brittonheadshot"} src={headshot} alt="photoguy"></img></div>
-                </section>
-            </div>
-        
-        
-        </div>
-        
-    )
-}
+  return (
+    <div className={"authorspage"}>
+      <h1 id={"mainh1"}>About the Authors</h1>
+      <div id={"authorcontainer"}>
+        <section className={"authorbox"}>
+          <h2>Xavier Reed</h2>
 
-export default Authorspage
+          <p>
+            Post bac in computer science in his last year. Enjoys reading,
+            hiking, programming, and being out in the sun.
+          </p>
+          <div className={"photoguy"}>
+            <img src="images/x.jpg" id="brittonheadshot" alt="photoguy"></img>
+          </div>
+        </section>
+        <section className={"authorbox"}>
+          <h2>Austin Britton</h2>
+
+          <p>
+            Computer Science Major with a passion for programming. Focused on
+            AI/ML in the area of game theory / decision making in games without
+            perfect information
+          </p>
+          <div className={"photoguy"}>
+            <img id={"brittonheadshot"} src={headshot} alt="photoguy"></img>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Authorspage;
